29b93e48f20c13ac850660394392d8f34e3f3999
denor1999 <ozhgixinv@list.ru>
2026-09-22 21:47:31 +0300

    fix bugs #1

--- src/test/java/models/pojo/RegistrationResponsePojoModel.java
+++ src/test/java/models/pojo/RegistrationResponsePojoModel.java
@@ -26,19 +26,19 @@ public class RegistrationResponsePojoModel {
         this.username = username;
     }
 
-    public String getFirstname() {
+    public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstname(String firstname) {
+    public void setFirstName(String firstname) {
         this.firstName = firstname;
     }
 
-    public String getLastname() {
+    public String getLastName() {
         return lastName;
     }
 
-    public void setLastname(String lastname) {
+    public void setLastName(String lastname) {
         this.lastName = lastname;
     }
 
