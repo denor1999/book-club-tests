fb8b9eb8567539b8344d569d7f8a7abe117cd883
denor1999 <ozhgixinv@list.ru>
2026-09-23 17:30:14 +0300

    added test data

---
+++ src/test/java/testdata/TestData.java
@@ -0,0 +1,17 @@
+package testdata;
+
+import net.datafaker.Faker;
+
+public class TestData {
+
+    private final Faker faker = new Faker();
+
+    public final String username = "qaguru";
+    public final String password = "qaguru123";
+    public final String wrongPassword = "qaguru1234";
+    public final String expectedTokenPath = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+    public final String expectedDetailError = "Invalid username or password.";
+    public final String expectedExistingUserError = "A user with that username already exists.";
+    public final String randomUsername = faker.name().firstName() + "_" + System.currentTimeMillis();
+    public final String randomPassword = faker.name().lastName();
+}
